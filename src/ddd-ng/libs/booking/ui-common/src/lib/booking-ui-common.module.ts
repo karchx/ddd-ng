c6dab09672d3749b03db2082aa9313e9fdf40b28
Стив <kenethra.74@gmail.com>
2022-11-24 22:27:41 -0600

    style: components material

--- src/ddd-ng/libs/booking/ui-common/src/lib/booking-ui-common.module.ts
+++ src/ddd-ng/libs/booking/ui-common/src/lib/booking-ui-common.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { FlightCardComponent } from './flight-card/flight-card.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+
+    MatButtonModule,
+    MatCardModule
+  ],
   declarations: [FlightCardComponent],
   exports: [FlightCardComponent],
 })
