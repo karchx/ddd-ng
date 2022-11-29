96f80a30a839ad0a54576a9d525059842a27845d
Стив <kenethra.74@gmail.com>
2022-11-28 19:06:25 -0600

    feat(booking): add feature

---
+++ src/ddd-ng/libs/booking/feature-book/src/lib/booking-feature-book.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightBookingComponent } from './flight-booking.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FlightBookingComponent],
+})
+export class BookingFeatureBookModule {}
---
+++ src/ddd-ng/libs/booking/feature-book/src/lib/flight-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ddd-ng-flight-booking',
+  templateUrl: './flight-booking.component.html',
+  styles: [],
+})
+export class FlightBookingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
